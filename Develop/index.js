63b21743ad5c34a0d24d1fea43fad53f573b9a60
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-01 19:51:24 -0400

    moved develop folder into new repo which has my inquirer prompt for questions

---
+++ Develop/index.js
@@ -0,0 +1,59 @@
+// Input
+// Prompt user for questions
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+
+  .then(function (response) {
+    console.log(response)
+    // Processing
+    // Figure out how to get answers to questions into markdown file create a 
+    
+    
+    // Output
+    // Give user markdown file
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
